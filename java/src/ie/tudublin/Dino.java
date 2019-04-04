90547c6a1357d4ce581e59f22cfe95a9f13701ab
Declan <Declan.thornee98@gmail.com>
2019-04-04 16:14:08 +0100

    Changed Dino variables

--- java/src/ie/tudublin/Dino.java
+++ java/src/ie/tudublin/Dino.java
@@ -5,7 +5,11 @@ import processing.core.PApplet;
 public class Dino
 {
     UI ui;
+    private float gap;
+    private float square;
     private float colorBars;
+    private float xBars;
+    private float randomColor;
 
     public Dino(UI ui)
     {
@@ -14,16 +18,19 @@ public class Dino
 
     public void render()
     {
-        colorBars = 70;
+        gap = ui.width/100;
+        square = (ui.height / 2) -50;
+        colorBars = 350f/11f;
+        xBars = 70;
        //Drawing Dinosaur box.
        ui.fill(0,222,0);
-       ui.rect(70, 100, 350, 250, 0, 0, 50 ,50);
+       ui.rect(70, 100, square, 250, 0, 0, 50 ,50);
        
        //Drawing color box above Dinosaur box.
-       for(int i = 0; i < 11; i++) {
-            
-            ui.rect(colorBars, 70, 350/11, 30);
-            colorBars += 70;
+       for(int i = 0; i < 11; i++) {    //loop creating top bar.
+        ui.fill(randomColor);
+        ui.rect(xBars, 80, colorBars, 20);
+        xBars = xBars + colorBars;
        }
 
     }
