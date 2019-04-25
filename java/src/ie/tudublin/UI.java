a406d2fefd80336c8817103d0c081a618c5faf5d
Declan <Declan.thornee98@gmail.com>
2019-04-25 10:04:59 +0100

    Added some comments

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -4,12 +4,14 @@ import processing.core.PApplet;
 
 public class UI extends PApplet
 {
+    //Declaring objects.
     Button b;
     MovingCircle mc; 
     Radar radar;
     MovingBar bars;
     Window view;
     
+    //Declaring variables.
     float outln = 10;
 
     public void settings() {
@@ -17,9 +19,8 @@ public class UI extends PApplet
     }
 
     public void setup() {
-        colorMode(HSB);
-        //b = new Button(this, 50, 50, 100, 50, "I am a button");
-        //mc = new MovingCircle(this, width / 2, height * .75f, 50);
+        colorMode(HSB);  //Changing colour mode.
+        //Creating instances of the classes.
         radar = new Radar(this, 1, width - 60 , height - 30, 10);
         bars = new MovingBar(this, (outln * 4), height - (outln * 2), (height/4f) * -1, width * .8f);
         view = new Window(this, width * .1f, width);
@@ -27,14 +28,10 @@ public class UI extends PApplet
     }
 
     public void draw() {
-        background(0,0,165);
-        backdrop();
-        
-        //b.render();
+        background(0,0,165); //Setting the backdrop to a grey.
+        backdrop(); //Calling backdrop() method.
 
-        //mc.update();
-        //mc.render();
-
+        //calling object methods.
         radar.update();
         radar.render();
 
@@ -46,6 +43,7 @@ public class UI extends PApplet
 
     }
 
+    //backdrop() method.
     public void backdrop() {
         
         stroke(127, 127, 127);
@@ -55,14 +53,11 @@ public class UI extends PApplet
         line(outln, outln, outln, height - outln);
         line(outln, height - outln, width - outln, height - outln);
         line(width - outln, height - outln, width - outln, outln);
-
+        //Drawing more lines to the screen.
         line( (outln * 2), outln, (outln * 2), height - (outln * 2) );
         line( (outln * 2), height - (outln * 2), width * .8f, height - (outln * 2) ); 
         line( width * .8f, height - (outln * 2), width * .8f + outln, height - (outln * 5) );
         line( width * .8f + outln, height - (outln * 5), width - outln  ,height - (outln * 5) );
     }
-
-	public void reset() {
-	}
 }
 
--- java/src/ie/tudublin/Window.java
+++ java/src/ie/tudublin/Window.java
@@ -7,51 +7,50 @@ public class Window
     UI ui;
     private float gap;
     private float xZ;
-    private float count = 100;
-    private float colour1 = 255;
-    private float colour2 = 255;
-    private float colour3 = 255;
-    private float x;
-    private float y;
-    private float z;
-    private  static float red;
-    private  static float blue;
+    private float xR = gap*.5f;
+
+    private static float red;
+    private static float blue;
 
     public Window(UI ui, float gap, float xZ)
     {
         this.ui = ui;
         this.gap = gap;
         this.xZ = xZ;
-
-        x = ui.random(0, xZ);
-        y = ui.random(0, xZ);
-        z = ui.random(0, xZ);
-
     }
 
     public void render()
     {
-       //Outline for colour box.
+        colourMouse();
+        colourBox();
+        bigBox();
+    }
+
+    public void update() {
+
+    }
+    
+    public void colourMouse() {
+        //Outline for colour box.
        red = ui.map(ui.mouseX, 0, ui.width, 0, 255);
        blue = ui.map(ui.mouseX, 0, ui.width, 255, 0);
+    }
+    public void colourBox() {
+       
        ui.fill(blue,red,red);
+       //Drawing colour bar above.
        ui.rect( gap*.5f, gap*.5f, xZ -gap, gap*.3f);
-       //Drawing Dinosaur box.
+    }
+
+    public void bigBox() {
+        
+        //Drawing Big box & outer box.
        ui.stroke(127, 127, 127);
        ui.strokeWeight(3);
        ui.fill(blue,red,red);
        ui.rect(gap - (gap*.3f), gap, ui.width - gap * 2, ui.height - 350);
        ui.fill(0);
        ui.rect(gap - (gap*.3f), gap, ui.width - gap * 2, ui.height - 350, 80, 80, 80 ,80);
-       
-       //ui.ellipse(ui.width - gap, ui.height - gap/3, 25, 25);
-    }
-
-    public void update()
-    {
-        if( ui.frameCount > count) {
-            count += 100;
-            colour1 = ui.random(0, 255); colour2 = ui.random(0, 255); colour3 = ui.random(0, 255);
-        }
+    
     }
 }
